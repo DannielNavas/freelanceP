8a1599d63d3b21701f1165617443a4fa8bf82da5
cesar mayorga <cesarleandromayorga7@gmail.com>
2018-08-18 20:08:31 -0500

    creacion de crud register
    crud register hecho

---
+++ contractusAPI/Modules/register.js
@@ -0,0 +1,166 @@
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "contractus"
+})
+
+connection.connect();
+
+var registerModel = {}
+
+// get users register
+registerModel.getUserRegister = function (userData, callback) {
+  console.log(userData)
+  var query = "select * from users "
+
+  if (connection) {
+    connection.query(query, function (error, rows) {
+      if (error) {
+        console.log(error)
+        callback(null, {
+          "respuesta": "Error de conexión"
+        })
+      } else {
+        if (rows.length != 0) {
+          var jsonObj = {
+            rows,
+            respuesta: "Success"
+          }
+          callback(null, jsonObj)
+        } else {
+          console.log("Error la consulta no arroja datos")
+          callback(null, {
+            "respuesta": "Error el usuario no se pudo consultar "
+          })
+        }
+      }
+    })
+  } else {
+    console.log("No se conecto con servidor")
+    callback(null, {
+      "Respuesta": "Error en Conexion"
+    })
+  }
+
+}
+
+
+// create users register
+registerModel.createUsersRegister = function (userData, callback) {
+  console.log(userData)
+  var query = "INSERT INTO users (email, pass) VALUES  ('" + userData.email + "',  '" + userData.pass + "') "
+
+  if (connection) {
+    connection.query(query, function (error, rows) {
+      if (error) {
+        console.log(error)
+        callback(null, {
+          "respuesta": "Error de conexión"
+        })
+      } else {
+        if (rows.length != 0) {
+          var jsonObj = {
+            rows,
+            respuesta: "Success"
+          }
+          callback(null, jsonObj)
+        } else {
+          console.log("Error la consulta no arroja datos")
+          callback(null, {
+            "respuesta": "Error el usuario no se pudo crear "
+          })
+        }
+      }
+    })
+  } else {
+    console.log("No se conecto con servidor")
+    callback(null, {
+      "Respuesta": "Error en Conexion"
+    })
+  }
+
+}
+
+
+// update users register
+registerModel.updateUsersRegister = function (userData, callback) {
+  console.log(userData)
+  var query = "UPDATE users SET email = '" + userData.email + "' , pass= '" + userData.pass + "'  where id='" + userData.id + "' "
+
+  if (connection) {
+    connection.query(query, function (error, rows) {
+      if (error) {
+        console.log(error)
+        callback(null, {
+          "respuesta": "Error de conexión"
+        })
+      } else {
+        if (rows.length != 0) {
+          var jsonObj = {
+            rows,
+            respuesta: "Success"
+          }
+          callback(null, jsonObj)
+        } else {
+          console.log("Error la consulta no arroja datos")
+          callback(null, {
+            "respuesta": "Error el usuario no se pudo actualizar "
+          })
+        }
+      }
+    })
+  } else {
+    console.log("No se conecto con servidor")
+    callback(null, {
+      "Respuesta": "Error en Conexion"
+    })
+  }
+
+}
+
+
+// delete users register
+registerModel.deleteUsersRegister = function (userData, callback) {
+  console.log(userData)
+  var query = "DELETE FROM users WHERE id='" + userData.id + "' "
+
+  if (connection) {
+    connection.query(query, function (error, rows) {
+      if (error) {
+        console.log(error)
+        callback(null, {
+          "respuesta": "Error de conexión"
+        })
+      } else {
+        if (rows.length != 0) {
+          var jsonObj = {
+            rows,
+            respuesta: "Success"
+          }
+          callback(null, jsonObj)
+        } else {
+          console.log("Error la consulta no arroja datos")
+          callback(null, {
+            "respuesta": "Error el usuario no se pudo eliminar "
+          })
+        }
+      }
+    })
+  } else {
+    console.log("No se conecto con servidor")
+    callback(null, {
+      "Respuesta": "Error en Conexion"
+    })
+  }
+
+}
+
+
+
+
+module.exports = registerModel;
+
+
+
