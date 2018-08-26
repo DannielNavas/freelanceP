f9e380d5caeb1693f7ca9d64376e097c00726a4d
Danniel Navas <darker13@outlook.com>
2018-08-25 21:13:07 -0500

    actualización
    Se entrega funciones que hacían falta  como la de crear usuario

--- contractusAPI/Modules/login.js
+++ contractusAPI/Modules/login.js
@@ -1,5 +1,8 @@
 var mysql = require('mysql')
+var enigma = require('enigma-code')
 
+const valorEncriptación = 10
+let key = 'Odin2019C'
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,9 +16,12 @@ connection.connect();
 var loginModel = {}
 
 loginModel.getUserLogin = function (userData, callback) {
-  console.log(userData)
-  var query = "select * from users where email = '" + userData.email + "' and pass = '" + userData.pass + "'"
-
+  var pass;
+  enigma.genHash(valorEncriptación, key, userData.pass, function (error, hash) {
+    if (error) return console.error(error)
+    pass = hash
+  })
+  var query = "select id, email, pass from users where email = '" + userData.email + "' and pass = '" + pass + "'"
   if (connection) {
     connection.query(query, function (error, rows) {
       if (error) {
@@ -45,6 +51,77 @@ loginModel.getUserLogin = function (userData, callback) {
     })
   }
 
+}
+
+
+
+loginModel.createUser = function (userData, callback) {
+  var pass;
+  enigma.genHash(valorEncriptación, key, userData.password, function (error, hash) {
+    if (error) return console.error(error)
+    pass = hash
+  })
+  var query = 'insert into users (email, user, pass) values ("' + userData.email + '","' + userData.user + '","' + pass + '");';
+  if (connection) {
+    connection.query(query, function (error, rows) {
+      if (error) {
+        console.log(error)
+        callback(null, {
+          "respuesta": "Error de conexión"
+        })
+      } else {
+        if (rows.length != 0) {
+          var jsonObj = {
+            respuesta: "Success"
+          }
+          callback(null, jsonObj)
+        } else {
+          console.log("Error la consulta no arroja datos")
+          callback(null, {
+            "respuesta": "Usuario y/o contraseña no son validos"
+          })
+        }
+      }
+    })
+  } else {
+    console.log("No se conecto con servidor")
+    callback(null, {
+      "Respuesta": "Error en Conexion"
+    })
+  }
+}
+
+
+loginModel.dataUser = function (userData, callback) {
+  var query = 'select * from users where id = ' + userData.id;
+  if (connection) {
+    connection.query(query, function (error, rows) {
+      if (error) {
+        console.log(error)
+        callback(null, {
+          "respuesta": "Error de conexión"
+        })
+      } else {
+        if (rows.length != 0) {
+          var jsonObj = {
+            rows,
+            respuesta: "Success"
+          }
+          callback(null, jsonObj)
+        } else {
+          console.log("Error la consulta no arroja datos")
+          callback(null, {
+            "respuesta": "Usuario y/o contraseña no son validos"
+          })
+        }
+      }
+    })
+  } else {
+    console.log("No se conecto con servidor")
+    callback(null, {
+      "Respuesta": "Error en Conexion"
+    })
+  }
 }
 
 module.exports = loginModel;
--- contractusAPI/routes/login.js
+++ contractusAPI/routes/login.js
@@ -6,14 +6,50 @@ var login = require('../Modules/login')
 
 
 router.post('/login', function (req, res, next) {
-  console.log(req.body.email)
-  console.log(req.body.password)
   var userData = {
     email: req.body.email,
     pass: req.body.password
   }
   login.getUserLogin(userData, function (error, data) {
-    res.status(200).jsonp(data)
+    if (error) {
+      res.status(501).jsonp({
+        "error": error
+      })
+    } else {
+      res.status(200).jsonp(data)
+    }
+  })
+})
+
+router.post('/createuser', function (req, res, next) {
+  var userData = {
+    email: req.body.email,
+    user: req.body.user,
+    password: req.body.password
+  }
+  login.createUser(userData, function (error, data) {
+    if (error) {
+      res.status(504).jsonp({
+        "error": error
+      })
+    } else {
+      res.status(200).jsonp(data)
+    }
+  })
+})
+
+router.post('/getDataUser', function (req, res, next) {
+  var userData = {
+    id: req.body.id
+  }
+  login.dataUser(userData, function (error, data) {
+    if (error) {
+      res.status(504).jsonp({
+        "error": error
+      })
+    } else {
+      res.status(200).jsonp(data)
+    }
   })
 })
 
